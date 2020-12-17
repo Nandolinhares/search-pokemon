c4dd7276ea68b2a512d2c193e54d71969affd712
Fernando Linhares <fernandolinhares1920@globo.com>
2020-12-17 17:57:06 -0300

    feat: app should start with initial state

--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Styles from './styles/global.scss'
+// MUI Stuff
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
 const App: React.FC = () => {
   return (
-    <section>
-      <img src="https://i.ibb.co/WnpST6t/pokeshop.png" alt="pokeshop" />
+    <section data-testid="main-section" className={Styles.mainSection}>
+      <img src="https://i.ibb.co/WnpST6t/pokeshop.png" alt="pokeshop" className={Styles.logo} />
+      <form className={Styles.form}>
+        <TextField id="outlined-basic" label="Pesquisar Pokemon" variant="outlined" />
+        <Button variant="contained" color="primary" >Pesquisar</Button>
+      </form>
     </section>
   )
 }
