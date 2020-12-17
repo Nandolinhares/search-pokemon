d3800240f8081dcd2480d242e513664538d2415c
Fernando Linhares <fernandolinhares1920@globo.com>
2020-12-17 11:45:29 -0300

    feat: created Search Pokemon and HttpPostClient interfaces, a HttpPostClientSpy and tested

---
+++ src/data/usecases/search-pokemon/remote-search.ts
@@ -0,0 +1,18 @@
+import { HttpPostClient } from '@/data/protocols/http/http-post-client'
+import { PokemonParams, SearchPokemon } from '@/domain/usecases/search-pokemon'
+
+export class RemoteSearch implements SearchPokemon {
+  constructor (
+    private readonly url: string,
+    private readonly httpPostClient: HttpPostClient
+  ) {}
+
+  async search (params: PokemonParams): Promise<object> {
+    await this.httpPostClient.post({
+      url: this.url,
+      body: params
+    })
+
+    return await Promise.resolve({})
+  }
+}
