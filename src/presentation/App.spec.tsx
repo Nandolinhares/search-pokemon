0796f4018513620c5b94cc0a2c5770455abe0528
Fernando Linhares <fernandolinhares1920@globo.com>
2020-12-17 18:19:57 -0300

    refactor: put SUT in a factory method

--- src/presentation/App.spec.tsx
+++ src/presentation/App.spec.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, cleanup, RenderResult } from '@testing-library/react'
 import App from './App'
+import { PokemonParams, SearchPokemon } from '@/domain/usecases/search-pokemon'
+
+class SearchPokemonSpy implements SearchPokemon {
+  async search (params: PokemonParams): Promise<object> {
+    return await Promise.resolve({})
+  }
+}
+
+type SutTypes = {
+  sut: RenderResult
+  searchPokemonSpy: SearchPokemonSpy
+}
+
+const makeSut = (): SutTypes => {
+  const sut = render(<App />)
+  const searchPokemonSpy = new SearchPokemonSpy()
+
+  return {
+    sut,
+    searchPokemonSpy
+  }
+}
 
 describe('App', () => {
+  afterEach(cleanup)
+
   test('Should start with initial state', () => {
-    const sut = render(<App />)
+    const { sut } = makeSut()
     const element = sut.getByTestId('main-section')
     expect(element.childElementCount).toBe(2)
   })
