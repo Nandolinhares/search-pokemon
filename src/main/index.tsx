58d80d080c629e00f693d6a266de7a210d87f5c5
Fernando Linhares <fernandolinhares1920@globo.com>
2020-12-17 16:18:49 -0300

    feat: created global sass css and its working

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from '@/presentation/App'
+// Styles
+import '@/presentation/styles/global.scss'
 
 ReactDOM.render(<App />, document.getElementById('main'))
