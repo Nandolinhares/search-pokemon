be1421d379c6c0ce9f927fcebee5031dbbbdc6eb
Fernando Linhares <fernandolinhares1920@globo.com>
2020-12-17 13:15:19 -0300

    test: ensure HttpPostClient throws NotFoundError if returned 404 statuscode

--- src/data/usecases/search-pokemon/remote-search.spec.ts
+++ src/data/usecases/search-pokemon/remote-search.spec.ts
@@ -2,34 +2,45 @@ import { HttpPostClientSpy } from '@/data/test/mock-http-client'
 import { RemoteSearch } from './remote-search'
 import { mockSearch } from '@/domain/test/mock-search'
 import { PokemonParams } from '@/domain/usecases/search-pokemon'
+import { NotFoundError } from '@/domain/errors/not-found-error'
 import faker from 'faker'
+import { HttpStatusCode } from '@/data/protocols/http/http-response'
 
 type SutParams = {
   sut: RemoteSearch
-  httpPostClient: HttpPostClientSpy<PokemonParams, object>
+  httpPostClientSpy: HttpPostClientSpy<PokemonParams, object>
 }
 
 const makeSut = (url: string = faker.internet.url()): SutParams => {
-  const httpPostClient = new HttpPostClientSpy<PokemonParams, object>()
-  const sut = new RemoteSearch(url, httpPostClient)
+  const httpPostClientSpy = new HttpPostClientSpy<PokemonParams, object>()
+  const sut = new RemoteSearch(url, httpPostClientSpy)
   return {
     sut,
-    httpPostClient
+    httpPostClientSpy
   }
 }
 
 describe('SearchPokemon', () => {
   test('Should call HttpPostClient with correct url', async () => {
     const url = faker.internet.url()
-    const { sut, httpPostClient } = makeSut(url)
+    const { sut, httpPostClientSpy } = makeSut(url)
     await sut.search(mockSearch())
-    expect(httpPostClient.url).toBe(url)
+    expect(httpPostClientSpy.url).toBe(url)
   })
 
   test('Should call HttpPostClient with correct body', async () => {
-    const { sut, httpPostClient } = makeSut()
+    const { sut, httpPostClientSpy } = makeSut()
     const body = mockSearch()
     await sut.search(body)
-    expect(httpPostClient.body).toEqual(body)
+    expect(httpPostClientSpy.body).toEqual(body)
+  })
+
+  test('Should throw NotFoundError if HttpPostClient returns 404', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.notFound
+    }
+    const promise = sut.search(mockSearch())
+    await expect(promise).rejects.toThrow(new NotFoundError())
   })
 })
