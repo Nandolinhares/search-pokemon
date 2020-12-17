33346abeb01161dbe3192f030aff12aaeb4b3d8a
Fernando Linhares <fernandolinhares1920@globo.com>
2020-12-17 12:03:27 -0300

    refactor: created a SUT implementation to a factory method

--- src/data/usecases/search-pokemon/remote-search.spec.ts
+++ src/data/usecases/search-pokemon/remote-search.spec.ts
@@ -1,23 +1,34 @@
 import { HttpPostClientSpy } from '@/data/test/mock-http-client'
 import { RemoteSearch } from './remote-search'
+import { mockSearch } from '@/domain/test/mock-search'
+import faker from 'faker'
+
+type SutParams = {
+  sut: RemoteSearch
+  httpPostClient: HttpPostClientSpy
+}
+
+const makeSut = (url: string = faker.internet.url()): SutParams => {
+  const httpPostClient = new HttpPostClientSpy()
+  const sut = new RemoteSearch(url, httpPostClient)
+  return {
+    sut,
+    httpPostClient
+  }
+}
 
 describe('SearchPokemon', () => {
   test('Should call HttpPostClient with correct url', async () => {
-    const url = 'any_url'
-    const httpPostClient = new HttpPostClientSpy()
-    const sut = new RemoteSearch(url, httpPostClient)
-    await sut.search({ name: 'Teste' })
+    const url = faker.internet.url()
+    const { sut, httpPostClient } = makeSut(url)
+    await sut.search(mockSearch())
     expect(httpPostClient.url).toBe(url)
   })
 
   test('Should call HttpPostClient with correct body', async () => {
-    const url = 'any_url'
-    const httpPostClient = new HttpPostClientSpy()
-    const sut = new RemoteSearch(url, httpPostClient)
-    const body = { name: 'Teste' }
+    const { sut, httpPostClient } = makeSut()
+    const body = mockSearch()
     await sut.search(body)
-    expect(httpPostClient.body).toEqual({
-      name: 'Teste'
-    })
+    expect(httpPostClient.body).toEqual(body)
   })
 })
